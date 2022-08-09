5f97ba3f11d003a029d23cac2016253602eb788e
Ana Caroline <caroline.ana.n.s@gmail.com>
2022-08-09 15:02:18 +0000

    Endpoint /oi created

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,14 @@ const express = require ("express");
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("Hello World");
+    res.send("Hello, World!");
 });
 
-app.listen(3000);
+app.get("/oi", function(req, res){
+    res.send("Olá, mundo!")
+})
+
+app.listen(3000, () =>
+    console.log("Servidor rodando em https://3000-anacaroline-oceanjornad-ev0lhfps24a.ws-us59.gitpod.io/")
+    
+);
